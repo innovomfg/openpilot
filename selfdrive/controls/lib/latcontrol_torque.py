2b5ef9a24500f7c807ab218aec32917219d6d679
innovomfg <67807414+innovomfg@users.noreply.github.com>
2024-08-30 09:02:45 -0400

    Update latcontrol_torque.py
    updated lowspeed_X & _Y

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -24,8 +24,8 @@ from openpilot.selfdrive.modeld.constants import ModelConstants
 # friction in the steering wheel that needs to be overcome to
 # move it at all, this is compensated for too.
 
-LOW_SPEED_X = [0, 10, 20, 30]
-LOW_SPEED_Y = [15, 13, 10, 5]
+LOW_SPEED_X = [0, 20, 35, 50]
+LOW_SPEED_Y = [17, 15, 12, 7]
 LOW_SPEED_Y_NN = [12, 3, 1, 0]
 
 LAT_PLAN_MIN_IDX = 5
