858bdcb70c82a135407b719c40435b49fecd69e1
innovomfg <67807414+innovomfg@users.noreply.github.com>
2025-11-12 11:22:17 -0500

    Update Hyundai Palisade specs in values.py
    Signed-off-by: innovomfg <67807414+innovomfg@users.noreply.github.com>

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -333,7 +333,7 @@ class CAR(Platforms):
       HyundaiCarDocs("Hyundai Palisade 2020-22", "All", video="https://youtu.be/TAnDqjF4fDY?t=456", car_parts=CarParts.common([CarHarness.hyundai_h])),
       HyundaiCarDocs("Kia Telluride 2020-22", "All", car_parts=CarParts([Device.threex_angled_mount, CarHarness.hyundai_h])),
     ],
-    CarSpecs(mass=1999, wheelbase=2.9, steerRatio=15.6 * 1.15, tireStiffnessFactor=0.63),
+    CarSpecs(mass=2184, wheelbase=2.9, steerRatio=18.4 * 1.15, tireStiffnessFactor=1.00),
     flags=HyundaiFlags.MANDO_RADAR | HyundaiFlags.CHECKSUM_CRC8,
   )
   HYUNDAI_VELOSTER = HyundaiPlatformConfig(
