4e2668552452672f4c669fd0ae144fc1dda68d88
innovomfg <67807414+innovomfg@users.noreply.github.com>
2026-03-15 17:44:46 -0400

    Adjust jerk and follow time factors for personalities
    Signed-off-by: innovomfg <67807414+innovomfg@users.noreply.github.com>

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -63,9 +63,9 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.0
+    return 0.75
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.5
+    return 0.45
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -74,9 +74,9 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45
+    return 1.15
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.25
+    return 0.27
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
